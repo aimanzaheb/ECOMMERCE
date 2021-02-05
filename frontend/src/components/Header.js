d2a7f74af51c583735b2f30b2197e05d3f814e16
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-05 20:02:48 +0530

    Product Detail Screen (Completed started frontend)
    Corrected  links not getting deactive in Header.js
    recommend useParams instead match
    https://css-tricks.com/the-hooks-of-react-router/

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,13 +13,13 @@ const Header = () => {
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
-                <Nav.Link>
+                <Nav.Link active={false}>
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i>Sign In
+                <Nav.Link active={false}>
+                  <i className='fas fa-user'></i> Sign In
                 </Nav.Link>
               </LinkContainer>
             </Nav>
