7190901a08c13773944a278a936d52badefcf042
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-05 16:16:47 +0530

    React Router
    Command:-
    npm i react-router-dom react-router-bootstrap
    Descriprion:-
    It uses html5 history api
    Use link component instead anchor tag for SPA  to prevent page reload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          {/* exact - prevent HomeScreen load on url '/anything' */}
+
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
