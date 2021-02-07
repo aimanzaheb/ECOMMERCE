09f5d6026f8d7f0484d5012c19e2dadb43bed3cd
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-07 16:51:03 +0530

    Conecting to database
    npm i mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  // used async because mongo db .connect, .find return promise
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }) //without these option we get errors in console
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1) //1 means exit with failure
+  }
+}
+
+export default connectDB
