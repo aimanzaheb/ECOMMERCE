cf721660938ef8b606cfcded216fd123a44667db
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-11 23:09:52 +0530

    Product details reducer & actions
    follow this order for convenience:-
    1. Add constants in productConstants.js
    2. define productDetails reducer in productReducers.js
    3. import productDetails reducer in store.js
    4. define listProductDetails action creator in productActions.js
    5. In productScreen.js import listProductDetails action creator and import useState & useDispatch hooks from react-redux
    
    Issue: -
    Previous product shows for a moment when see another product in browser

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch, getState) => {
@@ -52,3 +55,27 @@ export const listProducts = () => async (dispatch, getState) => {
 //     }
 //   }
 // }
+
+export const listProductDetails = (id) => async (dispatch, getState) => {
+  //this async returned func will be handle by redux-thunk
+  //will get fire from ProductScreen.js
+
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST }) //dispatch action object to reducer from redux-thunk
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message //our custom msg from backend
+          : error.message, //generic message
+    })
+  }
+}
