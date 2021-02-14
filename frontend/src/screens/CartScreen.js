38c80afb605424f84474002ca02bec70b60837d2
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-14 12:21:40 +0530

    User Authentication Endpoint

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
