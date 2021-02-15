867059ee442fdf3f3c3219ee98306c9cd9d69a0c
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-15 20:44:43 +0530

    Show user in navbar & logout
    Use userInfo redux state in Header.js
    
    Dispatch logout() action when click logout and redirect to '/login'

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,21 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom' //we can also withRouter
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap' //Similar to Link component from react-router-dom to prevent page refresh
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+    history.push('/login')
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -17,11 +31,22 @@ const Header = () => {
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link active={false}>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link active={false}>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -30,4 +55,5 @@ const Header = () => {
   )
 }
 
+//const HeaderWithRouter = withRouter(Header) //if using withRouter instead useHistory
 export default Header
