cb63e085c397c3fab4169f6dafc22d9b050bc7b7
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-15 19:27:12 +0530

    Login Screen & Functionality
    Create LoginScreen.js and add its route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
   return (
@@ -12,11 +13,13 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container fluid='xl'>
-          <Route path='/' component={HomeScreen} exact />
-          {/* exact - prevent HomeScreen load on url '/anything' */}
+          <Route path='/login' component={LoginScreen} />
           <Route path='/products/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           {/* cart id is optional */}
+
+          <Route path='/' component={HomeScreen} exact />
+          {/* exact - prevent HomeScreen load on url '/anything' */}
         </Container>
       </main>
       <Footer />
