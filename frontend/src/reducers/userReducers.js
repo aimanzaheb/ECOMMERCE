0c8aa23727cfbd900b749388b42c29e39b569eef
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-15 15:56:29 +0530

    User Login Reducer & Action
    Follow same pattern as in commit Product details reducer & actions
    
    Set useInfo to localStorage in userActions.js
    
    Set userInfo in initial state in store.js. It will overwrite initial userInfo set by userLoginReducer

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,23 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from '../constants/userConstants'
+
+export const userLoginReducer = (state = {}, action) => {
+  //passed previous/initial state
+
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true } //new state
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_LOGOUT:
+      return {}
+    default:
+      return state
+  }
+}
