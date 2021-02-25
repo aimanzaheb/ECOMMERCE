c4c68247cf8806f6e5518c754bc340cbffa5c960
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-25 20:01:39 +0530

    Checkout Steps Component
    We can also use react-stepper-horizonal for more beauty.
    Shipping screen is step 1 in CheckoutSteps.js
    Redirect ShippingScreen to login screen is user not logged in according to userLogin redux state

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
+  const { userInfo } = useSelector((state) => state.userLogin)
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    }
+  }, [userInfo, history])
+
   const cart = useSelector((state) => state.cart)
   const { shippingAddress } = cart
 
@@ -23,6 +31,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
