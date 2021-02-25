5e118d401ef1dae86b5af42b2c62daf6f596a27f
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-02-25 23:33:13 +0530

    Payment Screen & Save payment method
    Fixed bug : -
    In shipping screen used Redirect component to redirect  because if using history.push and redirect from payment screen to shipping screen "Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state."
    
    Always redirect in return if want to redirect before render

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -3,6 +3,7 @@ import {
   CART_REMOVE_ITEM,
   CART_EMPTY,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
 } from '../constants/cartConstants'
 
 export const cartReducer = (
@@ -41,6 +42,11 @@ export const cartReducer = (
       return {
         ...state,
         shippingAddress: action.payload,
+      }
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload,
       }
     case CART_EMPTY:
       return { cartItems: [], shippingAddress: {} }
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
@@ -7,11 +8,6 @@ import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
   const { userInfo } = useSelector((state) => state.userLogin)
-  useEffect(() => {
-    if (!userInfo) {
-      history.push('/login')
-    }
-  }, [userInfo, history])
 
   const cart = useSelector((state) => state.cart)
   const { shippingAddress } = cart
@@ -30,59 +26,65 @@ const ShippingScreen = ({ history }) => {
   }
 
   return (
-    <FormContainer>
-      <CheckoutSteps step1 />
-      <h1>Shipping</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='address'>
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter address'
-            value={address}
-            required
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+    <>
+      {/* always redirect here to prevent user from visit & redirect before rendering */}
+      {!userInfo && <Redirect to='/login' />}
+      {/* if using history.push and redirect from payment screen to shipping screen Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state. */}
 
-        <Form.Group controlId='city'>
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter city'
-            value={city}
-            required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+      <FormContainer>
+        <CheckoutSteps step1 />
+        <h1>Shipping</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='address'>
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter address'
+              value={address}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId='postalCode'>
-          <Form.Label>Postal Code</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter postal code'
-            value={postalCode}
-            required
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group controlId='city'>
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter city'
+              value={city}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId='country'>
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter country'
-            value={country}
-            required
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group controlId='postalCode'>
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter postal code'
+              value={postalCode}
+              required
+              onChange={(e) => setPostalCode(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Button type='submit' variant='primary'>
-          Continue
-        </Button>
-      </Form>
-    </FormContainer>
+          <Form.Group controlId='country'>
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter country'
+              value={country}
+              required
+              onChange={(e) => setCountry(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type='submit' variant='primary'>
+            Continue
+          </Button>
+        </Form>
+      </FormContainer>
+    </>
   )
 }
 
