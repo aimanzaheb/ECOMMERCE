2115a10261a1f6e96779295cde74c7cbdf1e0833
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-04-02 20:39:53 +0530

    User Edit Screen & Get User Details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,6 +2,7 @@ import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
+  USER_DETAILS_RESET,
   USER_LIST_FAIL,
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
@@ -64,6 +65,8 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     case USER_LOGOUT:
+      return { user: {} }
+    case USER_DETAILS_RESET:
       return { user: {} }
     default:
       return state
