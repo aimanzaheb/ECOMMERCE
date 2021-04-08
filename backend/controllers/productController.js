039fb23139a80e3f619c2f19603b46f07eb6fa65
aimanzaheb <50737539+aimanzaheb@users.noreply.github.com>
2021-04-08 22:23:58 +0530

    Create & Update Product Endpoint
    Only by admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -49,4 +49,63 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProductById, getProducts, deleteProduct }
+//  @desc     Create single product
+//  @route    POST /api/products
+//  @access   Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.userId,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+//  @desc     Update single product
+//  @route    PUT /api/products/:id
+//  @access   Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (!product) {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+
+  product.name = name || product.name
+  product.price = price || product.price
+  product.description = description || product.desciption
+  product.image = image || product.image
+  product.brand = brand || product.brand
+  product.category = category || product.category
+  product.countInStock = countInStock || product.countInStock
+
+  const updatedProduct = await product.save()
+  res.status(201).json(updatedProduct)
+})
+
+export {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
